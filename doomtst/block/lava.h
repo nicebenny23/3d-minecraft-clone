7a1a4b72dc692490ea78751bf1b9bc19b04f92e0
bcharnow09@gmail.com <bcharnow09@gmail.com>
2024-07-25 23:48:10 -0400

    qq

--- doomtst/block/lava.h
+++ doomtst/block/lava.h
@@ -1,1 +1,21 @@
-#pragma once
+#include "block.h"
+#include "liquid.h"
+#ifndef lava_HPP
+#define lava_HPP
+inline void lavainit(blockname::block* blk) {
+	blk->mesh.setfaces(lavatex, lavatex, lavatex, lavatex, lavatex, lavatex);
+	blk->solid = false;
+	blk->transparent = true;
+	blk->emitedlight = 0;
+	blk->mesh.scale = blockname::unitscale;
+	blk->addcomponent<liquidprop>(15);
+	blk->createaabb(true);
+
+	blk->minedfastwithpick = false;
+}
+inline void lavadelete(blockname::block* blk) {
+	blk->removecomponent<liquidprop>();
+	blk->removecomponent < aabb::Collider>();
+}
+
+#endif // !wood_HPP
